86393bafe7a3062263063d28aa4c2e98fc01c883
福田 拓郎 <76578159+takurofukuta@users.noreply.github.com>
2021-07-05 14:18:50 +0900

    CSSを適用

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,33 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="入力してください"></input>
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のタスク</p>
+      <div className="incompleted-task-area">
+        <p className="title">未完了のタスク</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>あああああああああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>いいいいいいいい</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>完了したタスク</p>
+      <div className="completed-task-area">
+        <p className="title">完了したタスク</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>ううううう</li>
             <button>戻す</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>えええええええ</li>
             <button>戻す</button>
           </div>
