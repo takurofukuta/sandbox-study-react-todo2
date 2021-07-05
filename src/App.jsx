21a064a87d63e1d78a0f3e10509e05e90e525d6f
福田 拓郎 <76578159+takurofukuta@users.noreply.github.com>
2021-07-05 14:12:32 +0900

    todoアプリの骨組みをHTMLで作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="入力してください"></input>
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のタスク</p>
+        <ul>
+          <div>
+            <li>あああああああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいいいいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了したタスク</p>
+        <ul>
+          <div>
+            <li>ううううう</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>えええええええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
